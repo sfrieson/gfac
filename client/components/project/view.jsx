66beb0abbc3fd7a17069ae3cbafc453e3ea67789
Steven Frieson <sfrieson@gmail.com>
2017-05-06 21:55:10 -0400

    Starts setting up Project edit.

--- client/components/project/view.jsx
+++ client/components/project/view.jsx
@@ -1,7 +1,19 @@
+import React from 'react'
 import { connect } from 'react-redux'
 
+import { Form } from '../common'
+import { Project } from '../models'
+
 export default connect(({ project, projectUpdate }) => ({project, projectUpdate}))(
-  function Project ({ project, projectUpdate, dispatch }) {
-    return <div>hi</div>
+  function ProjectView ({ project, projectUpdate, dispatch }) {
+    return <div>
+      <h2>Edit Project</h2>
+      <Form changeType='PROJECT_UPDATE' onSubmit={onSubmit} />
+    </div>
+
+    function onSubmit (e) {
+      e.preventDefault()
+      Project.update(project.id, projectUpdate)
+    }
   }
 )
