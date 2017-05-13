29f4943820c832b84babfde2972597c9f9671449
Steven Frieson <sfrieson@gmail.com>
2017-05-13 15:54:32 -0400

    Sets up basic search page with call.

--- client/components/pages/search/index.js
+++ client/components/pages/search/index.js
@@ -1,15 +1,40 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import dispatchAjax from 'utils/ajax-dispatch'
+import api from 'utils/api'
 import {
   Input
 } from 'common'
 
 export default connect(
   ({ searchForm, searchResults }) => ({searchForm, searchResults})
-)(function Search ({ searchForm, searchResults }) {
+)(function Search ({ searchForm = {query: ''}, searchResults, dispatch }) {
   return (
     <div>
-      <Input name='terms' type='text' value={searchForm && searchForm.terms} label='Search' />
+      <form onSubmit={onSubmit}>
+        <Input name='query' type='text' value={searchForm.query || ''} label='Search' onChange={onChange} />
+        <button className='btn'>Submit</button>
+      </form>
     </div>
   )
+
+  function onSubmit (e) {
+    e.preventDefault()
+    dispatchAjax('SEARCH', api(`
+      mutation Search ($query: String) {
+        search (query: $query) {
+          firstname
+          lastname
+          instagram
+        }
+      }
+    `, {search: searchForm}))
+  }
+
+  function onChange ({ target }) {
+    const change = {}
+    change[target.name] = target.value
+
+    dispatch({type: 'SEARCH_UPDATE', change})
+  }
 })
--- client/store/index.js
+++ client/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 
-const accountForm = function (state = {}, action) {
+const accountForm = (state = {}, action) => {
   switch (action.type) {
     case 'ACCOUNT_FORM_CHANGE':
       return {...state, ...action.change}
@@ -12,50 +12,7 @@ const accountForm = function (state = {}, action) {
   }
 }
 
-const nonprofitForm = function (state = {}, action) {
-  switch (action.type) {
-    case 'NONPROFIT_FORM_CHANGE':
-      return {...state, ...action.change}
-    case 'FETCH_NONPROFIT_UPDATE_END':
-      return {}
-    default:
-      return state
-  }
-}
-
-const projectForm = function (state = {}, action) {
-  switch (action.type) {
-    case 'PROJECT_FORM_CHANGE':
-      return {...state, ...action.change}
-    case 'FETCH_CREATE_PROJECT_END':
-      return {}
-    default:
-      return state
-  }
-}
-
-const ProjectUpdateRE = new RegExp('^PROJECT_UPDATE_(\\d*)')
-const projectUpdate = function (state = {}, action) {
-  const match = action.type.match(ProjectUpdateRE)
-  if (match) {
-    const id = match[1]
-    const projectState = Object.assign({}, state[id], action.change)
-    const newState = {...state}
-    newState[id] = projectState
-    return newState
-  } else return state
-}
-
-const projects = function (state = [], action) {
-  switch (action.type) {
-    case 'FETCH_PROJECTS_END':
-      return action.res.data.getProjects
-    default:
-      return state
-  }
-}
-
-const fetches = function (state = {}, action) {
+const fetches = (state = {}, action) => {
   if (!/^FETCH_/.test(action.type)) return state
   switch (action.type) {
     case 'FETCH_USER_END':
@@ -68,7 +25,7 @@ const fetches = function (state = {}, action) {
   }
 }
 
-const me = function (state = {}, action) {
+const me = (state = {}, action) => {
   switch (action.type) {
     case 'FETCH_UPDATE_ME_END':
     case 'FETCH_CREATE_PROJECT_END':
@@ -82,7 +39,68 @@ const me = function (state = {}, action) {
   }
 }
 
-const mainReducer = combineReducers({fetches, me, accountForm, nonprofitForm, projectForm, projectUpdate, projects})
+const nonprofitForm = (state = {}, action) => {
+  switch (action.type) {
+    case 'NONPROFIT_FORM_CHANGE':
+      return {...state, ...action.change}
+    case 'FETCH_NONPROFIT_UPDATE_END':
+      return {}
+    default:
+      return state
+  }
+}
+
+const projectForm = (state = {}, action) => {
+  switch (action.type) {
+    case 'PROJECT_FORM_CHANGE':
+      return {...state, ...action.change}
+    case 'FETCH_CREATE_PROJECT_END':
+      return {}
+    default:
+      return state
+  }
+}
+
+const ProjectUpdateRE = new RegExp('^PROJECT_UPDATE_(\\d*)')
+const projectUpdate = (state = {}, action) => {
+  const match = action.type.match(ProjectUpdateRE)
+  if (match) {
+    const id = match[1]
+    const projectState = Object.assign({}, state[id], action.change)
+    const newState = {...state}
+    newState[id] = projectState
+    return newState
+  } else return state
+}
+
+const projects = (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_PROJECTS_END':
+      return action.res.data.getProjects
+    default:
+      return state
+  }
+}
+
+const searchForm = (state = {}, action) => {
+  switch (action.type) {
+    case 'SEARCH_UPDATE':
+      return {...state, ...action.change}
+    default:
+      return state
+  }
+}
+
+const searchResults = (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_SEARCH_END':
+      return action.res.data.search
+    default:
+      return state
+  }
+}
+
+const mainReducer = combineReducers({fetches, me, accountForm, nonprofitForm, projectForm, projectUpdate, projects, searchForm, searchResults})
 
 const middleware = applyMiddleware(logger)
 const store = createStore(mainReducer, middleware)
