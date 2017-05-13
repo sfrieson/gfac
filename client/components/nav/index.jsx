05bfd7b2ff5ad6fdb53f820f96876d0ae7e8f0ff
Steven Frieson <sfrieson@gmail.com>
2017-05-13 14:10:26 -0400

    Adds user specific menu. Adds Admin's search.

--- client/components/nav/index.jsx
+++ client/components/nav/index.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
-
+import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import './styles.scss'
 
-export default function Header () {
+export default connect(({me}) => ({me}))(function Nav ({ me }) {
   return (
-    <div className='header col-sm-12 col-md-3'>
+    <nav className='header col-sm-12 col-md-3'>
       Menu
       <ul className='header'>
         <li><NavLink className='nav__link' activeClassName='nav__link--active' exact to='/' >Home</NavLink></li>
         <li><NavLink className='nav__link' activeClassName='nav__link--active' to='/account'>Account</NavLink></li>
-        <li><NavLink className='nav__link' activeClassName='nav__link--active' to='/project'>Your Projects</NavLink></li>
-        <li><NavLink className='nav__link' activeClassName='nav__link--active' to='/project/new'>New Project</NavLink></li>
+        {roleSpecificLinks(me.role)}
         <li><a href='/logout'>Logout</a></li>
       </ul>
-    </div>
+    </nav>
   )
+})
+
+function roleSpecificLinks (role) {
+  switch (role) {
+    case 'contact':
+      return [
+        <li key='projects'><NavLink className='nav__link' activeClassName='nav__link--active' to='/project'>Your Projects</NavLink></li>,
+        <li key='new project'><NavLink className='nav__link' activeClassName='nav__link--active' to='/project/new'>New Project</NavLink></li>
+      ]
+    case 'admin':
+      return <li key='search'><NavLink className='nav__link' activeClassName='nav__link--active' to='/search'>Search</NavLink></li>
+    default:
+      return null
+  }
 }
