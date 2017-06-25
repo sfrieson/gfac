3189db9f3aa15d72e77e3a17c593c3e6de23f6e1
Steven Frieson <sfrieson@gmail.com>
2017-06-25 17:18:06 -0400

    Adds projects for Admin

--- client/components/nav/index.jsx
+++ client/components/nav/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { NavLink } from 'react-router-dom'
+import { NavLink as RRNavLink } from 'react-router-dom'
 import './styles.scss'
 
+function NavLink (props) {
+  return <RRNavLink className='nav__link' activeClassName='nav__link--active' {...props} />
+}
 export default connect(({me}) => ({me}))(function Nav ({ me }) {
   return (
     <nav className='header col-sm-12 col-md-3'>
       Menu
       <ul className='header'>
-        <li><NavLink className='nav__link' activeClassName='nav__link--active' exact to='/' >Home</NavLink></li>
-        <li><NavLink className='nav__link' activeClassName='nav__link--active' to='/account'>Account</NavLink></li>
+        <li><NavLink exact to='/' >Home</NavLink></li>
+        <li><NavLink to='/account'>Account</NavLink></li>
         {roleSpecificLinks(me.role)}
         <li><a href='/logout'>Logout</a></li>
       </ul>
@@ -21,11 +24,14 @@ function roleSpecificLinks (role) {
   switch (role) {
     case 'contact':
       return [
-        <li key='projects'><NavLink className='nav__link' activeClassName='nav__link--active' to='/project'>Your Projects</NavLink></li>,
-        <li key='new project'><NavLink className='nav__link' activeClassName='nav__link--active' to='/project/new'>New Project</NavLink></li>
+        <li key='projects'><NavLink to='/project'>Your Projects</NavLink></li>,
+        <li key='new project'><NavLink to='/project/new'>New Project</NavLink></li>
       ]
     case 'admin':
-      return <li key='search'><NavLink className='nav__link' activeClassName='nav__link--active' to='/search'>Search</NavLink></li>
+      return [
+        <li key='projects'><NavLink to='/project'>Projects</NavLink></li>,
+        <li key='search'><NavLink to='/search'>Search</NavLink></li>
+      ]
     default:
       return null
   }
